6ac4c6e4b898e15087c3a08df4a07182ff9abef5
Aaron Estrada <rubenaaron.estradapoggio@unibz.it>
2016-12-10 23:14:20 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='IntAgACOfeature',
+    version='1.0',
+    packages=['', 'tests', 'acofeatures'],
+    url='',
+    license='',
+    author='Aaron Estrada',
+    author_email='aaron.estrada.poggio@gmail.com',
+    description='',
+    install_requires=['nltk', 'scikit-learn']
+)
